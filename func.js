59fe238127d36b7123a64384eb1058a92ad05b15
ak021 <akhilmp1721@gmail.com>
2023-06-03 11:25:26 +0530

    more notes

--- func.js
+++ func.js
@@ -1,15 +1,16 @@
 var x = 1;
 
+one();
+two();
+
 function one() {
   var x = 100;
   console.log(x);
 }
 
-function two() {
+var two = function () {
   var x = 200;
   console.log(x);
-}
+};
 
-one();
-two();
 console.log({ x });

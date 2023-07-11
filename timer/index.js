685e566fe26e8ca039d9105824deec54797d8a52
ak021 <akhilmp1721@gmail.com>
2023-07-11 07:16:47 +0530

    more

--- timer/index.js
+++ timer/index.js
@@ -5,11 +5,22 @@ class Timer {
     this.startButton = startButton;
     this.pauseButton = pauseButton;
     this.startButton.addEventListener("click", this.start);
+    this.pauseButton.addEventListener("click", this.pause);
     // this is a solution with bind
     // this.startButton.addEventListener("click", this.start.bind(this));
   }
   start = () => {
+    this.tick(); // otherwise it will wait 1 second to print the first tick, but we need to print the tick immediately.
     console.log("inside start");
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  tick = () => {
+    console.log("tick");
   };
 }
 
